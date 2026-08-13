1d48ab290e1faf027624485890c8e0b2445c4122
garycheahhaoyang <garychy-wm25@student.tarc.edu.my>
2026-08-13 09:15:43 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cvgwngvliq1p.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "garycheahhaoyang-employee"
+customregion = "us-east-1a"
+
